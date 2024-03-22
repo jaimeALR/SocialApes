e46ecf1e058f4368a577a86d971ca8653cf48065
jaimeALR <jaime_tuga@hotmail.com>
2024-03-22 11:24:05 +0000

    Community update

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,27 +1,29 @@
-import { ClerkProvider } from "@clerk/nextjs"
-import { Inter } from "next/font/google"
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
 
-import '../globals.css';
+import "../globals.css";
 
 export const metadata = {
-  title: 'Social Apes',
-  description: 'A Next.js 13 Meta Social Apes Application',
-}
+  title: "Social Apes",
+  description: "A Next.js 13 Meta Social Apes Application",
+};
 
-const inter = Inter({ subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
-      <body className={`${inter.className} bg-dark-1`}>
-        {children}
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center items-center min-h-screen">
+            {children}
+          </div>
         </body>
-    </html>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,11 @@
+const Page = () => {
+  return (
+    <div>
+      <section>
+        <h1 className="head-text mb-10"> Communities </h1>
+      </section>
+    </div>
+  );
+};
+
+export default Page;
